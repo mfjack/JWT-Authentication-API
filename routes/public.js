1288acc772f84a53453b067d3f450ef1b272f56e
Marlon Ferreira <ferreira.marlon@live.com>
2024-07-26 15:59:18 -0300

    feat: generate the token

--- routes/public.js
+++ routes/public.js
@@ -1,62 +1,65 @@
-import express from 'express'
-import bcrypt from 'bcrypt'
-import { PrismaClient } from '@prisma/client'
-import jwt from 'jsonwebtoken'
+import express from "express";
+import bcrypt from "bcrypt";
+import { PrismaClient } from "@prisma/client";
+import jwt from "jsonwebtoken";
 
-const prisma = new PrismaClient()
-const router = express.Router()
+const prisma = new PrismaClient();
+const router = express.Router();
 
-const JWT_SECRET = process.env.JWT_SECRET
+const JWT_SECRET = process.env.JWT_SECRET;
 
 // Cadastrar um novo usuário.
-router.post('/register', async (req, res) => {
+router.post("/register", async (req, res) => {
     try {
-        const user = req.body
-        const salt = await bcrypt.genSalt(10)
-        const hashPassword = await bcrypt.hash(user.password, salt)
+        const user = req.body;
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(user.password, salt);
 
         const userDB = await prisma.user.create({
             data: {
                 name: user.name,
                 email: user.email,
-                password: hashPassword
+                password: hashPassword,
                 // OBS: O retorno da senha é para fins didático.
-            }
-        })
-        res.status(201).json(userDB)
-    } catch (err) {
-        res.status(500).json({ message: 'Erro no servidor' })
+            },
+        });
+        res.status(201).json(userDB);
+    } catch (error) {
+        res.status(500).json({ message: "Erro no servidor." });
     }
-})
+});
 
 // Login do usuário.
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {
     try {
-        const userInfo = req.body
+        const userInfo = req.body;
 
         // Busca o usuário no banco de dados.
         const user = await prisma.user.findUnique({
             where: {
-                email: userInfo.email
-            }
-        })
+                email: userInfo.email,
+            },
+        });
 
         // Verifica se o usuário existe no banco de dados.
         if (!user) {
-            return res.status(404).json({ message: 'Usuário não encontrado' })
+            return res.status(404).json({ message: "Usuário não encontrado." });
         }
 
         // Verifica se a senha confere com a senha do usuário no banco de dados.
-        const isMatch = await bcrypt.compare(userInfo.password, user.password)
+        const isMatch = await bcrypt.compare(userInfo.password, user.password);
 
         if (!isMatch) {
-            return res.status(401).json({ message: 'Credenciais inválidas' })
+            return res.status(401).json({ message: "Credenciais inválidas." });
         }
 
-        res.status(200).json(user)
-    } catch (err) {
-        res.status(500).json({ message: 'Erro no servidor' })
-    }
-})
+        // Gerar o token JWT.
+        const token = jwt.sign({ id: user.id }, JWT_SECRET, { expiresIn: "1d" });
 
-export default router
+        res.status(200).json(token);
+    } catch (err) {
+        res.status(500).json({ message: "Erro no servidor." });
+    }
+});
+
+export default router;
